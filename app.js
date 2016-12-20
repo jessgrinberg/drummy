3a5b5fa32fb90f69a058059418fcd7cb97e7881c
Jessica Grinberg <jessgrinberg@gmail.com>
2016-12-20 12:51:49 -0800

    added js logic

---
+++ app.js
@@ -0,0 +1,21 @@
+function makeSound(e){
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	if(!audio) return;
+	audio.currentTime = 0;
+	audio.play();
+	key.classList.add('playing');
+}
+
+function removeTransition(e) {
+	if(e.propertyName !== 'transform') return;
+	this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+for (var i=0; i<keys.length; i++) {
+	keys[i].addEventListener('transitionend', removeTransition)
+}
+
+window.addEventListener('keydown', makeSound);
+
